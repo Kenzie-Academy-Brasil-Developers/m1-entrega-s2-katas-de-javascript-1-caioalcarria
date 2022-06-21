a0edef06fdbf6e9c2a9baa6ae8efd29ea7385669
Caio Alcarria <101140937+caioalcarria@users.noreply.github.com>
2022-06-21 17:33:20 -0300

    final

--- katas1.js
+++ katas1.js
@@ -1,167 +1,202 @@
 function oneThroughTwenty() {
+
+    let array = []
+    let num = 0
     
+    while (num < 20) {
+        num++
+        array.push(num)   
+
+
+    }
+    console.log(array)
+    return array
+    
+    
+}
+
+ 
+ //call function oneThroughTwenty
+ 
+ 
+ function evensToTwenty() {
+   let array = [] 
    let num=0
-while(num<20){
-    num++
-    console.log(num)
+ while(num<20){
+     num++
+     if(num%2==0){
+        array.push(num)
+     }
+     
+    
+ }
+ console.log(array)
+    return array
    
-}
+ }
+ 
+ //call function evensToTwenty
+ 
+ 
+ function oddsToTwenty() {
+    let array = [] 
+ let num=0
+ while(num<20){
+     num++
+     if(num%2!=0){
+        array.push(num)
+     }
+     
     
-}
+ }
+ console.log(array)
+    return array
+ }
+ 
+ //call function oddsToTwenty
 
-//call function oneThroughTwenty
-
-function evensToTwenty() {
+ 
+ 
+ function multiplesOfFive() {
+    let array = [] 
+ let num=0
+ while(num<100){
+     num++
+     if(num%5==0){
+        array.push(num)
+     }
+     
     
-  let num=0
-while(num<20){
-    num++
-    if(num%2==0){
-        console.log(num)
-    }
+ }
+ console.log(array)
+    return array
+ }
+ 
+ //call function multiplesOfFive
+ 
+ 
+ function squareNumbers() {
+    let array = []
+ let num=0
+ while(num<100){
+     num++
+     let quadrado = num**2
+     if(quadrado<=100){
+        array.push(quadrado)
+     }
+     
     
-   
-}
-  
-}
-
-//call function evensToTwenty
-
-function oddsToTwenty() {
+ }
+ console.log(array)
+    return array
+ }
+ 
+ //call function squareNumbers
+ 
+ 
+ function countingBackwards() {
+    let array = []
+ let num=20
+ while(num>=1){
+     
+     
+    array.push(num)
+     
+     num= num-1
+ 
+     
     
-let num=0
-while(num<20){
-    num++
-    if(num%2!=0){
-        console.log(num)
-    }
+ }
+ console.log(array)
+    return array
+ }
+ 
+ //call function countingBackwards
+ 
+ 
+ function evenNumbersBackwards() {
+    let array = []
+ let num=20
+ while(num>=1){
+     
+     if(num%2==0){
+        array.push(num)
+     }
+     
+     
+     num= num-1
+ 
     
-   
-}
-}
-
-//call function oddsToTwenty
-
-function multiplesOfFive() {
+ }
+ console.log(array)
+    return array
+ }
+ 
+ //call function evenNumbersBackwards
+ 
+ function oddNumbersBackwards() {
+    let array = []
+ let num=20
+ while(num>=1){
+     
+     if(num%2!=0){
+        array.push(num)
+     }
+     
+     
+     num= num-1
+ 
     
-let num=0
-while(num<100){
-    num++
-    if(num%5==0){
-        console.log(num)
-    }
-    
-   
-}
-}
-
-//call function multiplesOfFive
-
-function squareNumbers() {
-    
-let num=0
-while(num<100){
-    num++
-    let quadrado = num**2
-    if(quadrado<=100){
-        console.log(quadrado)
-    }
-    
-   
-}
-}
-
-//call function squareNumbers
-
-function countingBackwards() {
-    
-let num=20
-while(num>=1){
-    
-    
-    console.log(num)
-    
-    num= num-1
-
-    
-   
-}
-}
-
-//call function countingBackwards
-
-function evenNumbersBackwards() {
-    
-let num=20
-while(num>=1){
-    
-    if(num%2==0){
-        console.log(num)
-    }
-    
-    
-    num= num-1
-
-   
-}
-}
-
-//call function evenNumbersBackwards
-
-function oddNumbersBackwards() {
-    
-let num=20
-while(num>=1){
-    
-    if(num%2!=0){
-        console.log(num)
-    }
-    
-    
-    num= num-1
-
-   
-}
-}
-
-//call function oddNumbersBackwards
-
-function multiplesOfFiveBackwards() {
-    
+ }
+ console.log(array)
+    return array
+ }
+ 
+ //call function oddNumbersBackwards
+ function multiplesOfFiveBackwards() {
+    let array = [] 
  let num=100
-while(num>=1){
+ while(num>1){
+     num=num-1
+     if(num%5==0){
+        array.push(num)
+     }
+     
     
-    if(num%5!=0){
-        console.log(num)
+ }
+ console.log(array)
+    return array
+ }
+ 
+ 
+ //call function multiplesOfFiveBackwards
+ 
+ multiplesOfFiveBackwards()
+ 
+ 
+ 
+ 
+ 
+ function squareNumbersBackwards() {
+    let array = []
+   let num=100
+ while(num>=1){
+     
+     let quadrado = num**2
+ 
+    if(quadrado<=100){
+        array.push(quadrado)
     }
     
+     
+     
+     
+     num= num-1
+ 
     
-    num= num-1
-
-   
-}
-}
-
-//call function multiplesOfFiveBackwards
-
-function squareNumbersBackwards() {
-    
-  let num=100
-while(num>=1){
-    
-    let quadrado = num**2
-
-   if(quadrado<=100){
-         console.log(quadrado)
-   }
-   
-    
-    
-    
-    num= num-1
-
-   
-}
-}
-
-//call function squareNumbersBackwards
+ }
+ console.log(array)
+    return array
+ }
+ 
+ //call function squareNumbersBackwards
